5f322e17970848e29f43d832bc6e5bdf6bf4b905
kbmann3521 <160088830+kbmann3521@users.noreply.github.com>
2025-02-12 12:31:54 -0500

    Delete image-manipulator directory

--- image-manipulator/index.js
+++
@@ -1,8 +0,0 @@
-// index.js - Simple Node.js Script
-
-function getCurrentDateTime() {
-    const now = new Date();
-    return now.toLocaleString();
-}
-
-console.log("Hello! The current date and time is:", getCurrentDateTime());
